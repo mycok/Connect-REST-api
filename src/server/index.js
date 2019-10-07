f7f144a82d12b66f1a1678da42454a39b2ce3e79
mycok <awesomeme155@gmail.com>
2019-10-07 21:13:04 +0300

    ch-(mongoDB) integrate mongoDB
    - Install and integrate mongoDB
    - Add both development and production mongoDB configurations
    - Install and configure mongoose
    
    [Delivers #168966400]

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,21 @@
+import mongoose from 'mongoose';
+
 import app from './express';
 import config from '../config';
 import logger from '../utils/logger';
 
-const { port } = config;
+const { port, mongoUri } = config;
+
+mongoose.Promise = global.Promise;
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(`${mongoUri}`);
+mongoose.connection.on('error', () => {
+  throw new Error(`Unable to connect to database ${mongoUri}`);
+});
+
 app.listen(port, (error) => {
   if (error) {
     logger.alert(error);
